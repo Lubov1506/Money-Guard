84613d8ee30c7126282326f9270c1893124656b3
AnnetaDe <anneta.liss@gmail.com>
2024-05-31 01:22:49 +0200

    1

---
+++ src/components/StatisticsDashboard/StatisticDatePicker.jsx
@@ -0,0 +1,99 @@
+import 'react-datepicker/dist/react-datepicker.css';
+import { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import s from './StatisticDatePicker.module.css';
+import { getYear } from 'date-fns';
+import Select from 'react-select';
+
+export const StatisticDatePicker = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const years = Array.from(
+    { length: getYear(new Date()) - 2000 + 1 },
+    (_, i) => 2000 + i
+  );
+  const months = Array.from({ length: 12 }, (_, i) => i + 1);
+  const handleYearChange = selectedOption => {
+    const newDate = new Date(selectedDate.setYear(selectedOption.value));
+    setSelectedDate(newDate);
+    console.log(selectedOption);
+  };
+  const handleMonthChange = selectedOption => {
+    const newDate = new Date(selectedDate.setMonth(selectedOption.value - 1));
+
+    setSelectedDate(newDate);
+    console.log(newDate);
+    console.log(selectedOption);
+  };
+
+  const yearOptions = years.map(year => ({ value: year, label: year }));
+  const monthOptions = months.map(month => ({
+    value: month.toLocaleString('en-US', { minimumIntegerDigits: 2 }),
+    label: month.toLocaleString('en-US', { minimumIntegerDigits: 2 }),
+  }));
+
+  return (
+    <div>
+      <Select
+        options={yearOptions}
+        onChange={handleYearChange}
+        placeholder="Select year"
+        value={{ value: getYear(selectedDate), label: getYear(selectedDate) }}
+      />
+      <Select
+        options={monthOptions}
+        onChange={handleMonthChange}
+        placeholder="Select month"
+        value={{
+          value: new Date(selectedDate).toLocaleString('en-US', {
+            month: 'long',
+          }),
+          label: new Date(selectedDate).toLocaleString('en-US', {
+            month: 'long',
+          }),
+        }}
+      />
+    </div>
+  );
+
+  //   const renderMonthContent = (month, shortMonth, longMonth) => {
+  //     const tooltipText = `Tooltip for month: ${longMonth} `;
+  //     return <span title={tooltipText}>{longMonth}</span>;
+  //   };
+  //   const renderYearContent = year => {
+  //     const tooltipText = `${year}`;
+  //     return <span title={tooltipText}>{year}</span>;
+  //   };
+
+  //   return (
+  //     <div>
+  //       <CalendarContainer className={s.calendar}>
+  //         <DatePicker
+  //           renderYearContent={renderYearContent}
+  //           renderCustomHeader={() => <span></span>}
+  //           selected={startDate}
+  //           onChange={date => setStartDate(date)}
+  //           dateFormat="yyyy"
+  //           showYearPicker
+  //           minDate={new Date(2000, 0, 1)}
+  //           maxDate={new Date()}
+  //           placeholderText="Select year"
+  //           shouldCloseOnSelect={true}
+  //           scrollableYearDropdown={true}
+  //           yearItemNumber={12}
+  //         />
+
+  //         <DatePicker
+  //           selected={startDate}
+  //           renderMonthContent={renderMonthContent}
+  //           onChange={date => setStartDate(date)}
+  //           dateFormat="MMMM"
+  //           showMonthYearPicker
+  //           renderCustomHeader={() => <span></span>}
+  //           maxDate={new Date()}
+  //           placeholderText="Select month"
+  //           shouldCloseOnSelect={true}
+  //         />
+  //       </CalendarContainer>
+  //     </div>
+  //   );
+};
