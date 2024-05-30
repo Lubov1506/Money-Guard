5e7773f3aae3cb08e61039040373791cfda5c759
Dimka863 <dlysachenko98@gmail.com>
2024-05-30 13:58:30 +0200

    ops

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,5 @@
-import { useDispatch } from 'react-redux';
-import {
-  signInThunk,
-  signOutThunk,
-  signUpThunk,
-} from './redux/auth/operations';
-
 function App() {
-  const dispatch = useDispatch();
-  return (
-    <>
-      <button
-        type="button"
-        onClick={() =>
-          dispatch(
-            signUpThunk({
-              username: 'Dimk',
-              email: 'dlysachenko@gmail.comse',
-              password: 'dlysachenko@gmail.comse',
-            })
-          )
-        }
-      >
-        Sign-up
-      </button>
-      <button
-        type="button"
-        onClick={() =>
-          dispatch(
-            signInThunk({
-              email: 'dlysachenko@gmail.comse',
-              password: 'dlysachenko@gmail.comse',
-            })
-          )
-        }
-      >
-        sign-in
-      </button>
-      <button
-        type="button"
-        onClick={() => dispatch(signOutThunk())}
-      >
-        sign-out
-      </button>
-      <button type="button"></button>
-      <button type="button"></button>
-      <button type="button"></button>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
