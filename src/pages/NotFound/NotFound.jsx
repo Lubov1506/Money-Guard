5a9d774b83f3c20d4894aa34059f59ad77014906
AnnaHtslk <hannah.htslk@gmail.com>
2024-05-31 18:25:26 +0300

    added icon and link

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,22 +1,17 @@
 import s from "./NotFound.module.css";
-import { selectIsLoggedIn } from "../../redux/auth/selectors";
-import { useSelector } from "react-redux";
+
 import { Link } from "react-router-dom";
 
 const NotFound = () => {
-  const isLoggedIn = useSelector(selectIsLoggedIn);
   return (
     <div className={s.wrapper}>
-      <p className={s.error}>The page you requested does not exist</p>
-      {isLoggedIn ? (
-        <Link className={s.link} to="/home">
-          Home
-        </Link>
-      ) : (
-        <Link className={s.link} to="/">
-          Log in
-        </Link>
-      )}
+      <div className={s.content}>
+        <img src="/errorgirl.webp" alt="page-not-found" width="320" />
+        <p className={s.error}>Woops, the page does not exist</p>
+      </div>
+      <Link className={s.link} to="/">
+        Home
+      </Link>
     </div>
   );
 };
