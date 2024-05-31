913263623895a7b0bfeb146f6f70373ba6bfe848
AnnaHtslk <hannah.htslk@gmail.com>
2024-05-31 13:11:52 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import PrivateRoute from "./routes/PrivateRoute";
 import PublicRoute from "./routes/PublicRoute";
 import Dashboard from "./pages/Dashboard/Dashboard";
+import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
   return (
@@ -14,16 +15,16 @@ function App() {
           </PrivateRoute>
         }
       >
-        <Route index element={<HomeTab />} />
-        <Route path="statistics" element={<StatisticsTab />} />
-        <Route path="currency" element={<CurrencyTab />} />
+        <Route index element={<p>HomeTab</p>} />
+        <Route path="statistics" element={<p>StatisticsTab</p>} />
+        <Route path="currency" element={<p>CurrencyTab</p>} />
       </Route>
 
       <Route
         path="register"
         element={
           <PublicRoute>
-            <RegistrationPage />
+            <p>RegistrationPage</p>
           </PublicRoute>
         }
       />
@@ -31,11 +32,11 @@ function App() {
         path="login"
         element={
           <PublicRoute>
-            <LoginPage />
+            <p>LoginPage</p>
           </PublicRoute>
         }
       />
-      <Route path="*" element={<p>Not Found Page</p>} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
