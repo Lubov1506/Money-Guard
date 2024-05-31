39eb93f3a812f4d25f397f9a8e8dc966e4e4b553
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2024-05-31 13:48:52 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import PrivateRoute from "./routes/PrivateRoute";
 import PublicRoute from "./routes/PublicRoute";
 import Dashboard from "./pages/Dashboard/Dashboard";
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectIsRefreshing } from './redux/auth/selectors';
-import { selectIsLoading } from './redux/transactions/selectors';
-import { refreshUserThunk } from './redux/auth/operations';
-import Loader from './components/Loader/Loader';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectIsRefreshing } from "./redux/auth/selectors";
+import { selectIsLoading } from "./redux/transactions/selectors";
+import { refreshUserThunk } from "./redux/auth/operations";
+import Loader from "./components/Loader/Loader";
 
 function App() {
   const dispatch = useDispatch();
@@ -18,40 +18,42 @@ function App() {
     dispatch(refreshUserThunk());
   }, [dispatch]);
   return (
-    <>{(isLoading || isRefreshing) && <Loader />}
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <PrivateRoute>
-            <Dashboard />
-          </PrivateRoute>
-        }
-      >
-        <Route index element={<p>HomeTab</p>} />
-        <Route path="statistics" element={<p>StatisticsTab</p>} />
-        <Route path="currency" element={<p>CurrencyTab</p>} />
-      </Route>
+    <>
+      {(isLoading || isRefreshing) && <Loader />}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        >
+          <Route index element={<p>HomeTab</p>} />
+          <Route path="statistics" element={<p>StatisticsTab</p>} />
+          <Route path="currency" element={<p>CurrencyTab</p>} />
+        </Route>
 
-      <Route
-        path="register"
-        element={
-          <PublicRoute>
-            <p>RegistrationPage</p>
-          </PublicRoute>
-        }
-      />
-      <Route
-        path="login"
-        element={
-          <PublicRoute>
-            <p>LoginPage</p>
-          </PublicRoute>
-        }
-      />
-      <Route path="*" element={<p>NotFoundPage</p>} />
-    </Routes>
-  )</>
-
+        <Route
+          path="register"
+          element={
+            <PublicRoute>
+              <p>RegistrationPage</p>
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="login"
+          element={
+            <PublicRoute>
+              <p>LoginPage</p>
+            </PublicRoute>
+          }
+        />
+        <Route path="*" element={<p>NotFoundPage</p>} />
+      </Routes>
+    </>
+  );
+}
 
 export default App;
