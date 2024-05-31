36210009d3fa3ebae04df4e5cdcdc94a01230977
AnnaHtslk <hannah.htslk@gmail.com>
2024-05-31 16:28:48 +0300

    add links to NotFound

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectIsRefreshing } from './redux/auth/selectors';
-import { selectIsLoading } from './redux/transactions/selectors';
-import { refreshUserThunk } from './redux/auth/operations';
-import Loader from './components/Loader/Loader';
-import Currency from './components/Currency/Currency';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectIsRefreshing } from "./redux/auth/selectors";
+import { selectIsLoading } from "./redux/transactions/selectors";
+import { refreshUserThunk } from "./redux/auth/operations";
+import Loader from "./components/Loader/Loader";
+// import Currency from "./components/Currency/Currency";
+import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
   const dispatch = useDispatch();
@@ -14,7 +15,12 @@ function App() {
   useEffect(() => {
     dispatch(refreshUserThunk());
   }, [dispatch]);
-  return <>{(isLoading || isRefreshing) && <Loader />}</>;
+  return (
+    <>
+      {(isLoading || isRefreshing) && <Loader />}
+      <NotFound />
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,24 @@
+import s from "./NotFound.module.css";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  return (
+    <div className={s.wrapper}>
+      <p>The page you requested does not exist</p>
+      {isLoggedIn ? (
+        <Link className={s.link} to="/home">
+          Home
+        </Link>
+      ) : (
+        <Link className={s.link} to="/">
+          Log in
+        </Link>
+      )}
+    </div>
+  );
+};
+
+export default NotFound;
