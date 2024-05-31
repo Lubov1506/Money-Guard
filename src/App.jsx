a4f89265ef655ad2a920f51c7d60ce86c59faadf
Yelyzaveta31 <Didenkoelizabeth@gmail.com>
2024-05-31 13:11:52 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectIsRefreshing } from './redux/auth/selectors';
-import { selectIsLoading } from './redux/transactions/selectors';
-import { refreshUserThunk } from './redux/auth/operations';
-import Loader from './components/Loader/Loader';
-import Currency from './components/Currency/Currency';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectIsRefreshing } from "./redux/auth/selectors";
+import { selectIsLoading } from "./redux/transactions/selectors";
+import { refreshUserThunk } from "./redux/auth/operations";
+import Loader from "./components/Loader/Loader";
+import Currency from "./components/Currency/Currency";
 
 function App() {
   const dispatch = useDispatch();
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,12 +1,13 @@
-import { Outlet } from 'react-router-dom';
-import s from './Dashboard.module.css';
-import { useMediaQuery } from 'react-responsive';
+import { Outlet } from "react-router-dom";
+import s from "./Dashboard.module.css";
+import { useMediaQuery } from "react-responsive";
+import Header from "../../components/Header/Header";
 
 const Dashboard = () => {
   const isDesktop = useMediaQuery({
-    query: '(min-width: 1280px)',
+    query: "(min-width: 1280px)",
   });
-  const isTablet = useMediaQuery({ query: '(min-width: 768px)' });
+  const isTablet = useMediaQuery({ query: "(min-width: 768px)" });
 
   return (
     <>
