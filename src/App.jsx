756df717fa1abdda97309772e7e6b1ffee8b753b
AnnaHtslk <hannah.htslk@gmail.com>
2024-05-31 19:00:08 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import { Route, Routes } from 'react-router-dom';
 import PrivateRoute from './routes/PrivateRoute';
 import PublicRoute from './routes/PublicRoute';
-import Dashboard from './pages/Dashboard/Dashboard';
+// import Dashboard from './pages/Dashboard/Dashboard';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectIsRefreshing } from './redux/auth/selectors';
 import { selectIsLoading } from './redux/transactions/selectors';
 import { refreshUserThunk } from './redux/auth/operations';
 import Loader from './components/Loader/Loader';
-import RegistrationPage from './pages/RegistrationPage';
-import LoginPage from './pages/LoginPage';
-import CurrencyTab from './components/CurrencyTab/CurrencyTab';
+// import RegistrationPage from './pages/RegistrationPage';
+// import LoginPage from './pages/LoginPage';
+// import CurrencyTab from './components/CurrencyTab/CurrencyTab';
+import { lazy, Suspense } from 'react';
+
+const Dashboard = lazy(() => import('./pages/Dashboard/Dashboard'));
+// const HomeTab = lazy(() => import('./HomeTab'));
+// const StatisticsTab = lazy(() => import('./StatisticsTab'));
+const CurrencyTab = lazy(() => import('./components/CurrencyTab/CurrencyTab'));
+const RegistrationPage = lazy(() => import('./pages/RegistrationPage'));
+const LoginPage = lazy(() => import('./pages/LoginPage'));
+// const NotFound = lazy(() => import('./NotFound/'));
 
 function App() {
   const dispatch = useDispatch();
@@ -23,38 +32,40 @@ function App() {
   return (
     <>
       {(isLoading || isRefreshing) && <Loader />}
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        >
-          <Route index element={<p>HomeTab</p>} />
-          <Route path="statistics" element={<p>StatisticsTab</p>} />
-          <Route path="currency" element={<CurrencyTab/>} />
-        </Route>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          >
+            <Route index element={<p>HomeTab</p>} />
+            <Route path="statistics" element={<p>StatisticsTab</p>} />
+            <Route path="currency" element={<CurrencyTab />} />
+          </Route>
 
-        <Route
-          path="register"
-          element={
-            <PublicRoute>
-              <RegistrationPage/>
-            </PublicRoute>
-          }
-        />
-        <Route
-          path="login"
-          element={
-            <PublicRoute>
-              <LoginPage/>
-            </PublicRoute>
-          }
-        />
-        <Route path="*" element={<p>NotFoundPage</p>} />
-      </Routes>
+          <Route
+            path="register"
+            element={
+              <PublicRoute>
+                <RegistrationPage />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="login"
+            element={
+              <PublicRoute>
+                <LoginPage />
+              </PublicRoute>
+            }
+          />
+          <Route path="*" element={<p>NotFoundPage</p>} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
