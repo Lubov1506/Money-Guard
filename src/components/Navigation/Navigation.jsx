bb6f64a56252ee5c6f83a360a6693d99bc8be41c
Kosdevua <konstantin.vodolozhko@gmail.com>
2024-06-01 03:25:21 +0300

    added naviagation icons

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,8 +1,38 @@
+import { NavLink } from 'react-router-dom';
+import icons from '../../images/icons/sprite.svg';
+import s from './Navigation.module.css';
+import clsx from 'clsx';
+
+const buildLinkClass = ({ isActive }) => {
+  return clsx(s.link, isActive && s.active);
+};
 
 const Navigation = () => {
   return (
-    <div>Navigation</div>
-  )
-}
+    <ul className={s.link_icon_wrapper}>
+      <li>
+        <NavLink to="/" exact className={buildLinkClass}>
+          <svg className={s.icon}>
+            <use href={`${icons}#icon-home`} />
+          </svg>
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/statistics" className={buildLinkClass}>
+          <svg className={s.icon}>
+            <use href={`${icons}#icon-statistics`} />
+          </svg>
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/currency" className={buildLinkClass}>
+          <svg className={s.icon}>
+            <use href={`${icons}#icon-currency`} />
+          </svg>
+        </NavLink>
+      </li>
+    </ul>
+  );
+};
 
-export default Navigation
+export default Navigation;
