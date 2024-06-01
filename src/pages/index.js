2eb6ee093eb495e67c2351842e0802fe995b05e9
AnnaHtslk <hannah.htslk@gmail.com>
2024-06-01 14:07:42 +0300

    reexported files (pages, routes)

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as RegistrationPage } from './RegistrationPage';
+export { default as LoginPage } from './LoginPage';
+export { default as Dashboard } from './Dashboard/Dashboard';
+export { default as NotFound } from './NotFound/NotFound';
