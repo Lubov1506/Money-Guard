6cf169e94572ca813e63c7c6a23d72a7ad28d9e0
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2024-06-01 08:51:20 +0300

    Update StatisticsTab.jsx

--- src/components/StatisticsTab/StatisticsTab.jsx
+++ src/components/StatisticsTab/StatisticsTab.jsx
@@ -1,16 +1,9 @@
-import { useMediaQuery } from "react-responsive";
 import DoughnutChart from "../DoughnutChart/DoughnutChart"
 import { StatisticDatePicker } from "../StatisticsDashboard/StatisticDatePicker"
 import { StatisticsTable } from "../StatisticsTable/StatisticsTable"
 import s from './StatisticsTab.module.css'
 
 const StatisticsTab = () => {
-      const isDesktop = useMediaQuery({
-    query: '(min-width: 1280px)',
-  });
-  const isTablet = useMediaQuery({
-    query: '(min-width: 768px)',
-  });
   return (
       <div className={s.stat_wrap}>
           <DoughnutChart />
