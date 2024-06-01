e117d2ceccb884deaa1e75812a47eca9ba768b22
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2024-06-01 09:00:11 +0300

    Update StatisticsTab.jsx

--- src/components/StatisticsTab/StatisticsTab.jsx
+++ src/components/StatisticsTab/StatisticsTab.jsx
@@ -1,18 +1,18 @@
-import DoughnutChart from "../DoughnutChart/DoughnutChart"
-import { StatisticDatePicker } from "../StatisticsDashboard/StatisticDatePicker"
-import { StatisticsTable } from "../StatisticsTable/StatisticsTable"
-import s from './StatisticsTab.module.css'
+import DoughnutChart from '../DoughnutChart/DoughnutChart';
+import { StatisticDatePicker } from '../StatisticsDashboard/StatisticDatePicker';
+import { StatisticsTable } from '../StatisticsTable/StatisticsTable';
+import s from './StatisticsTab.module.css';
 
 const StatisticsTab = () => {
   return (
-      <div className={s.stat_wrap}>
-          <DoughnutChart />
-          <div>
-              <StatisticDatePicker />
-              <StatisticsTable/>
-          </div>
+    <div className={s.stat_wrap}>
+      <DoughnutChart />
+      <div>
+        <StatisticDatePicker />
+        <StatisticsTable />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default StatisticsTab
+export default StatisticsTab;
