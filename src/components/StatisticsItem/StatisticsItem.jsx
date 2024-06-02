910466dfd384fea1f6b02adb8dbbd140d859a1a4
AnnetaDe <anneta.liss@gmail.com>
2024-06-02 03:30:12 +0200

    s

--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,15 +1,17 @@
 import s from './StatisticsItem.module.css';
 const StatisticsItem = ({ item }) => {
   return (
-    <tr className={s.item}>
-      <th scope="row" className={s.row}>
-        <td className={s.td}>
-          <span className={s.color}></span>
-        </td>
-        <td className={s.name}>{item.name}</td>
-
-        <td className={s.td}>{item.total}</td>
-      </th>
+    <tr className={s.row}>
+      <td className={s.td}>
+        <span
+          className={s.color}
+          style={{
+            backgroundColor: item.color,
+          }}
+        ></span>
+      </td>
+      <td className={s.name}>{item.name}</td>
+      <td className={s.total}>{item.total}</td>
     </tr>
   );
 };
--- src/components/StatisticsTable/StatisticsTable.jsx
+++ src/components/StatisticsTable/StatisticsTable.jsx
@@ -2,69 +2,63 @@ import { useSelector } from 'react-redux';
 import StatisticsItem from '../StatisticsItem/StatisticsItem';
 import s from './StatisticsTable.module.css';
 import { selectPeriodTransactions } from '../../redux/transactions/selectors';
+import prettyMoneyFormat from '../../constants/PrettyMoneyFormat';
+import { getTrasactionCategoryColor } from '../../constants/TransactionConstants';
 
 const StatisticsTable = () => {
   const transactions = useSelector(selectPeriodTransactions);
-  // console.log(transactions);
-
+  console.log(transactions);
   const { expenseSummary, incomeSummary, categoriesSummary } = transactions;
-  //   console.log({ expenseSummary, incomeSummary, categoriesSummary });
+  const data =
+    categoriesSummary && categoriesSummary.length > 0
+      ? categoriesSummary
+          .map(item => {
+            return {
+              ...item,
+              total: prettyMoneyFormat(item.total),
+              color: getTrasactionCategoryColor(item.name),
+            };
+          })
+          .filter(item => item.type === 'EXPENSE')
+      : null;
+  const incomeData =
+    incomeSummary && incomeSummary >= 0
+      ? prettyMoneyFormat(incomeSummary)
+      : null;
+  const expenseData = expenseSummary ? prettyMoneyFormat(expenseSummary) : null;
 
-  // console.log(expenseSummary);
-  // console.log(incomeSummary);
-  // console.log(categoriesSummary);
+  console.log(data, incomeData, expenseData);
 
-  return (
-    <div className={s.all}>
-      <table className={s.table}>
-<<<<<<< Updated upstream
-        <thead className={s.thread}>
-          <tr>
-            <th scope="col" className={s.th}>
-              Category
-            </th>
-            <th scope="col" className={s.th}>
-              Total
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {categoriesSummary
-            ? categoriesSummary.map((item, idx) => (
-                <StatisticsItem key={idx} item={item} />
-              ))
-            : null}
-          <tr className={s.expences}>
-            <td> Expenses: {expenseSummary}</td>
-          </tr>
-          <tr className={s.income}>
-            <td> Income: {incomeSummary}</td>
-          </tr>
-        </tbody>
-=======
-        <tr className={s.heading}>
-          <th className={s.th}>Category</th>
-          <th className={s.th}>Total</th>
-        </tr>
-
-        {categoriesSummary
-          ? categoriesSummary.map((item, idx) => (
+  if (data) {
+    return (
+      <div className={s.all}>
+        <table className={s.table}>
+          <thead className={s.heading}>
+            <tr>
+              <th className={s.category}>Category</th>
+              <th className={s.sum}>Total</th>
+            </tr>
+          </thead>
+          <tbody className={s.tableBody}>
+            {data.map((item, idx) => (
               <StatisticsItem key={idx} item={item} />
-            ))
-          : null}
+            ))}
+          </tbody>
+          <tfoot className={s.tableFooter}>
+            <tr className={s.expences}>
+              <th className={s.exText}>Expences:</th>
+              <th>{expenseData}</th>
+            </tr>
 
-        <tr className={s.expences}>
-          <th className={s.exText}>Expences:</th>
-          <th>{expenseSummary}</th>
-        </tr>
-
-        <tr className={s.income}>
-          <th className={s.inText}>Income:</th>
-          <th>{incomeSummary}</th>
-        </tr>
->>>>>>> Stashed changes
-      </table>
-    </div>
-  );
+            <tr className={s.income}>
+              <th className={s.inText}>Income:</th>
+              <th>{incomeData}</th>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    );
+  }
+  return null;
 };
 export default StatisticsTable;
