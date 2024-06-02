686706eb25d3370cdeca8676f237ecc3919aa801
AnnetaDe <anneta.liss@gmail.com>
2024-06-02 23:06:16 +0200

    s

--- src/components/StatisticDatePicker/StatisticDatePicker.jsx
+++ src/components/StatisticDatePicker/StatisticDatePicker.jsx
@@ -12,12 +12,9 @@ const StatisticDatePicker = () => {
   const [selectedYear, setSelectedYear] = useState(currentYear);
   const [selectedMonth, setSelectedMonth] = useState(null);
   const dispatch = useDispatch();
-
   useEffect(() => {
     dispatch(fetchPeriodTrnThunk({ year: selectedYear, month: selectedMonth }));
   }, [selectedMonth, selectedYear, dispatch]);
-  console.log(currentMonth);
-
   const years = Array.from(
     { length: getYear(new Date()) - 2020 + 1 },
     (_, i) => 2020 + i
@@ -31,66 +28,68 @@ const StatisticDatePicker = () => {
       isDisabled: i + 1 > currentMonth && selectedYear === currentYear,
     };
   });
-  return (
-    <div>
-      <div className={s.monthYearPick}>
-        <Select
-          styles={{
-            control: (baseStyles, state) => ({
-              ...baseStyles,
-              borderColor: state.isFocused ? 'grey' : 'red',
-              backgroundColor: 'rgba(74, 86, 226, 0.10)',
-              color: 'white',
-            }),
-            dropdownIndicator: baseStyles => ({
-              ...baseStyles,
-              color: 'white',
-            }),
-            menu: baseStyles => ({
-              ...baseStyles,
-              backgroundColor: 'rgba(83, 61, 186, 1)',
-            }),
-            option: (baseStyles, state) => ({
-              ...baseStyles,
-              color: state.isSelected ? '#FF868D' : 'white',
-            }),
-            singleValue: baseStyles => ({
-              ...baseStyles,
-              color: 'white',
-            }),
+  const customStyles = {
+    control: (baseStyles, state) => ({
+      ...baseStyles,
+      borderColor: state.isFocused ? 'grey' : ' rgba(74, 86, 226, 0.10)',
+      backgroundColor: 'transparent',
+      color: 'white',
+      borderCollapse: 'separate',
+    }),
+    dropdownIndicator: baseStyles => ({
+      ...baseStyles,
+      color: 'white',
+    }),
+    menu: baseStyles => ({
+      ...baseStyles,
+      background:
+        'linear-gradient(180deg, #855daf99 13%, #6A46A5 53%, #50309A 70%, #6A46A5 100%)',
+      borderCollapse: 'separate',
+      borderRadius: '8px',
+      boxShadow: '0px 4px 60px 0px rgba(0, 0, 0, 0.25)',
+      backdropFilter: 'blur(50px)',
+    }),
+    option: (baseStyles, state) => ({
+      ...baseStyles,
+      backgroundColor: state.isFocused ? '#ffffff1a' : 'transparent',
+      color: state.isSelected ? '#FF868D' : 'white',
+    }),
+    singleValue: baseStyles => ({
+      ...baseStyles,
+      color: 'white',
+    }),
 
-            cursor: 'pointer',
-          }}
-          onChange={selectedOption => setSelectedYear(selectedOption.value)}
-          placeholder="Select year"
-          options={yearOptions}
-        />
-        <Select
-          styles={{
-            control: (baseStyles, state) => ({
-              ...baseStyles,
-              borderColor: state.isFocused ? 'grey' : 'red',
-              backgroundColor: 'rgba(74, 86, 226, 0.10)',
-            }),
-            dropdownIndicator: baseStyles => ({
-              ...baseStyles,
-              color: 'white',
-            }),
-            menu: baseStyles => ({
-              ...baseStyles,
-              backgroundColor: 'rgba(74, 86, 226, 1)',
-            }),
-          }}
-          onChange={selectedOption => setSelectedMonth(selectedOption.value)}
-          options={monthsOptions}
-          placeholder="Select month"
-        />
-      </div>
-      <div></div>
+    indicatorSeparator: baseStyles => ({
+      ...baseStyles,
+      display: 'none',
+    }),
+    valueContainer: baseStyles => ({
+      ...baseStyles,
+      padding: '12px 0px 12px 20px',
+    }),
+
+    cursor: 'pointer',
+  };
+
+  return (
+    <div className={s.monthYearPick}>
+      <Select
+        defaultValue={currentYear}
+        styles={customStyles}
+        onChange={selectedOption => setSelectedYear(selectedOption.value)}
+        openMenuOnClick={true}
+        placeholder="Select year"
+        options={yearOptions}
+      />
+      <Select
+        styles={customStyles}
+        onChange={selectedOption => setSelectedMonth(selectedOption.value)}
+        options={monthsOptions}
+        placeholder="Select month"
+      />
     </div>
   );
 };
-
 export default StatisticDatePicker;
 
 // import 'react-datepicker/dist/react-datepicker.css';
