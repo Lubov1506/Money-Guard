93f1c41cf7af9f738bd17efd0b200f3f88e1abb5
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2024-06-02 10:52:13 +0300

    add some styles

--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -2,15 +2,19 @@ import s from './StatisticsItem.module.css';
 const StatisticsItem = ({ item }) => {
   return (
     <tr className={s.row}>
-      <td className={s.td}>
+      {/* <td className={s.td}>
+     
+      </td> */}
+      <td className={s.name}>
+
         <span
           className={s.color}
           style={{
             backgroundColor: item.color,
           }}
         ></span>
+        {item.name}
       </td>
-      <td className={s.name}>{item.name}</td>
       <td className={s.total}>{item.total}</td>
     </tr>
   );
