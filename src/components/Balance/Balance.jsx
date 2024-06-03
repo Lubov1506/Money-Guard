1d62173ecb771320cba170bc7e173e6744f00962
Kosdevua <konstantin.vodolozhko@gmail.com>
2024-06-03 10:35:06 +0300

    Update Balance.jsx

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,13 +1,28 @@
-// import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { getBalanceThunk } from '../../redux/auth/operations';
+import { useEffect } from 'react';
 import s from './Balance.module.css';
+import { useLocation } from 'react-router-dom';
+
 const Balance = () => {
-  // const totalBalance = useSelector;
-  // state => state.finance.totalBalance;
+  const dispatch = useDispatch();
+  const balance = useSelector(state => state.auth.user.balance);
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === '/') {
+      dispatch(getBalanceThunk());
+    }
+  }, [dispatch, location.pathname]);
+
+  if (location.pathname !== '/') {
+    return null;
+  }
+
   return (
     <div className={s.balance_container}>
       <p className={s.balance_label}>Your balance</p>
-      <p className={s.balance_amount}>₴{'24 000.00'}</p>
-      {/* <p className={s.balance_amount}>₴{totalBalance}</p> */}
+      <p className={s.balance_amount}>₴ {balance?.toFixed(2)}</p>
     </div>
   );
 };
