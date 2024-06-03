478ff94070c053ba59a87a4b02dd144fc5cce718
Sergii Minko <minkosergii@gmail.com>
2024-06-03 22:56:33 +0200

    balance-tmp

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,11 +1,17 @@
 import { useSelector } from 'react-redux';
 import s from './Balance.module.css';
 import { getformatNumber } from 'helpers';
-import { selectBalance } from '../../redux/auth/selectors';
+// import { selectBalance } from '../../redux/auth/selectors';
+import { selectPeriodTransactions } from '../../redux/transactions/selectors'; 
 import clsx from 'clsx';
 
 const Balance = () => {
-  const balance = useSelector(selectBalance);
+  // const balance = useSelector(selectBalance);
+  const transactions = useSelector(selectPeriodTransactions);
+  const expenseTotal = transactions.expenseSummary || 0;
+  const incomeTotal = transactions.incomeSummary || 0;
+  const total = incomeTotal + expenseTotal;
+  const balance = total ? total : 0;
 
   const formattedNumber = getformatNumber(balance);
 
