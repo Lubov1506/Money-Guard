3445c0545d03361a76bcd774e3aaf1d474b66733
AnnetaDe <anneta.liss@gmail.com>
2024-06-03 12:45:30 +0200

    Update StatisticDatePicker.jsx
    all year cjeck it

--- src/components/StatisticDatePicker/StatisticDatePicker.jsx
+++ src/components/StatisticDatePicker/StatisticDatePicker.jsx
@@ -24,54 +24,18 @@ const StatisticDatePicker = () => {
   console.log(yearOptions);
   const monthsOptions = Array.from({ length: 12 }, (e, i) => {
     const month = new Date(0, i).toLocaleString('en', { month: 'long' });
+    console.log(month);
     return {
       value: i + 1,
       label: month,
       isDisabled: i + 1 > currentMonth && selectedYear === currentYear,
     };
   });
-  // const customStyles = {
-  //   control: (baseStyles, state) => ({
-  //     ...baseStyles,
-  //     borderColor: state.isFocused ? 'grey' : ' rgba(74, 86, 226, 0.10)',
-  //     backgroundColor: 'transparent',
-  //     color: 'white',
-  //     borderCollapse: 'separate',
-  //   }),
-  //   dropdownIndicator: baseStyles => ({
-  //     ...baseStyles,
-  //     color: 'white',
-  //   }),
-  //   menu: baseStyles => ({
-  //     ...baseStyles,
-  //     background:
-  //       'linear-gradient(180deg, #855daf99 13%, #6A46A5 53%, #50309A 70%, #6A46A5 100%)',
-  //     borderCollapse: 'separate',
-  //     borderRadius: '8px',
-  //     boxShadow: '0px 4px 60px 0px rgba(0, 0, 0, 0.25)',
-  //     backdropFilter: 'blur(50px)',
-  //   }),
-  //   option: (baseStyles, state) => ({
-  //     ...baseStyles,
-  //     backgroundColor: state.isFocused ? '#ffffff1a' : 'transparent',
-  //     color: state.isSelected ? '#FF868D' : 'white',
-  //   }),
-  //   singleValue: baseStyles => ({
-  //     ...baseStyles,
-  //     color: 'white',
-  //   }),
+  monthsOptions.push({ value: null, label: 'All time' });
 
-  //   indicatorSeparator: baseStyles => ({
-  //     ...baseStyles,
-  //     display: 'none',
-  //   }),
-  //   valueContainer: baseStyles => ({
-  //     ...baseStyles,
-  //     padding: '16px 0px 16px 20px',
-  //   }),
-
-  //   cursor: 'pointer',
-  // };
+  const onChange = inputValue => {
+    setSelectedMonth(inputValue);
+  };
 
   return (
     <div className={s.monthYearPick_wrapper}>
@@ -85,10 +49,9 @@ const StatisticDatePicker = () => {
         options={yearOptions}
       />
       <Select
-        isClearable={true}
         styles={datePickerStyles}
         className={s.monthYearPick}
-        onChange={selectedOption => setSelectedMonth(selectedOption.value)}
+        onChange={option => onChange(option.value, option.action)}
         options={monthsOptions}
         placeholder="Select month"
       />
