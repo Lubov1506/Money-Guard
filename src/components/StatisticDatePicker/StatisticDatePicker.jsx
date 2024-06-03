169690988de20bd02f64c55faf9a608ba9d4fdb9
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2024-06-03 22:25:23 +0300

    try framer

--- src/components/StatisticDatePicker/StatisticDatePicker.jsx
+++ src/components/StatisticDatePicker/StatisticDatePicker.jsx
@@ -6,6 +6,45 @@ import Select from 'react-select';
 import { useDispatch } from 'react-redux';
 import { fetchPeriodTrnThunk } from '../../redux/transactions/operations';
 import { datePickerStyles } from './datePickerStyles';
+import { useAnimate, stagger, motion } from "framer-motion";
+
+const staggerMenuItems = stagger(0.1, { startDelay: 0.15 });
+
+function useMenuAnimation(isOpen) {
+  const [scope, animate] = useAnimate();
+
+  useEffect(() => {
+    animate(".arrow", { rotate: isOpen ? 180 : 0 }, { duration: 0.2 });
+
+    animate(
+      "ul",
+      {
+        clipPath: isOpen
+          ? "inset(0% 0% 0% 0% round 10px)"
+          : "inset(10% 50% 90% 50% round 10px)",
+      },
+      {
+        type: "spring",
+        bounce: 0,
+        duration: 0.5,
+      }
+    );
+
+    animate(
+      "li",
+      isOpen
+        ? { opacity: 1, scale: 1, filter: "blur(0px)" }
+        : { opacity: 0, scale: 0.3, filter: "blur(20px)" },
+      {
+        duration: 0.2,
+        delay: isOpen ? staggerMenuItems : 0,
+      }
+    );
+  }, [isOpen]);
+
+  return scope;
+}
+
 
 const StatisticDatePicker = () => {
   const currentMonth = new Date().getMonth() + 1;
@@ -31,10 +70,18 @@ const StatisticDatePicker = () => {
     };
   });
   monthsOptions.unshift({ value: null, label: 'All months' });
+  const [scope, animate] = useAnimate();
 
+  useEffect(() => {
+    animate(
+      // scope.current,
+      // { opacity: [0, 1], y: [-10, 0] },
+      // { delay: stagger(0.1) }
+    );
+  }, [scope, animate]);
   return (
-    <div className={s.monthYearPick_wrapper}>
-      <Select
+    <motion.div className={s.monthYearPick_wrapper}>
+      <Select ref={scope}
         defaultValue={{ value: null, label: 'All months' }}
         styles={datePickerStyles}
         className={s.monthYearPick}
@@ -51,7 +98,7 @@ const StatisticDatePicker = () => {
         placeholder="Select year"
         options={yearOptions}
       />
-    </div>
+    </motion.div>
   );
 };
 export default StatisticDatePicker;
