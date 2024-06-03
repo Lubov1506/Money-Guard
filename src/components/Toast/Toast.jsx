5a12d0ecc5ac3605ff765da3c861a9d1f0da264d
AnnetaDe <anneta.liss@gmail.com>
2024-06-03 13:28:44 +0200

    1

--- src/components/Toast/Toast.jsx
+++ src/components/Toast/Toast.jsx
@@ -1,6 +1,11 @@
 import { Toast, ToastContainer } from 'react-toastify/dist/components';
 import 'react-toastify/dist/ReactToastify.css';
-const Toast = () => {
-  return <div>Toast</div>;
-};
+
+const Toast = ({ closeToast, toastProps }) => (
+  <div>
+    Lorem ipsum dolor {toastProps.position}
+    <button>Retry</button>
+    <button onClick={closeToast}>Close</button>
+  </div>
+);
 export default Toast;
