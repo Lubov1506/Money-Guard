d3d440439dec45cbd32dfd8a2b0dd9e04eb38809
Yelyzaveta31 <Didenkoelizabeth@gmail.com>
2024-06-03 13:40:54 +0300

    add localStorage.clear

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,32 +7,39 @@ import s from './Header.module.css';
 import Loader from '../Loader/Loader';
 import Logo from '../common/Logo/Logo';
 import { useMedia } from '../../hooks/useMedia';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const user = useSelector(selectUser);
   const userName = user ? user.email.split('@')[0] : '';
-
   const { isMobile } = useMedia();
+  const navigate = useNavigate();
+
   const open = () => {
     setIsModalOpen(true);
-    console.log('modal is open');
   };
 
   const close = () => {
     setIsModalOpen(false);
   };
 
+  const handleLogout = async () => {
+    try {
+      localStorage.clear();
+      navigate('/login');
+    } catch (error) {
+      alert('Failed to exit. Try again');
+    }
+  };
   return (
     <>
       <header className={s.header}>
-
-          <Logo
-            type="header"
-            width={isMobile ? 18 : 25}
-            height={isMobile ? 18 : 23}
-          />
-
+        <Logo
+          type="header"
+          width={isMobile ? 18 : 25}
+          height={isMobile ? 18 : 23}
+        />
 
         <div className={s.user}>
           <span className={s.userName}>{userName}</span>
@@ -41,13 +48,13 @@ const Header = () => {
             {!isMobile && <p>Exit</p>}
           </button>
         </div>
-
-        {isModalOpen && (
-          <Suspense fallback={<Loader />}>
-            <LogoutModal onClose={close} />
-          </Suspense>
-        )}
       </header>
+
+      {isModalOpen && (
+        <Suspense fallback={<Loader />}>
+          <LogoutModal onClose={close} onLogout={handleLogout} />
+        </Suspense>
+      )}
     </>
   );
 };
