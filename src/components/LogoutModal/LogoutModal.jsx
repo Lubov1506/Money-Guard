d18629f66369461c721a36e871a6ac5427b8599b
Dimka863 <dlysachenko98@gmail.com>
2024-06-03 13:08:44 +0200

    some more upd for logout

--- src/components/LogoutModal/LogoutModal.jsx
+++ src/components/LogoutModal/LogoutModal.jsx
@@ -4,34 +4,31 @@ import { signOutThunk } from '../../redux/auth/operations';
 import FormButton from '../common/FormButton/FormButton';
 import { useMedia } from 'hooks';
 import Logo from '../common/Logo/Logo';
-import Modal from 'components/Modal/Modal';
 
 const LogoutModal = ({ onClose }) => {
   const dispatch = useDispatch();
   const { isMobile } = useMedia();
 
   return (
-    <Modal onClose={onClose}>
-      <div className={s.modal}>
-        {!isMobile && <Logo />}
-        <h3 className={s.text}>Are you sure you want to log out?</h3>
-        <div className={s.btns}>
-          <FormButton
-            type="button"
-            text={'Logout'}
-            variant={'multiColorButtton'}
-            handlerFunction={() => dispatch(signOutThunk())}
-          />
+    <div className={s.modal}>
+      {!isMobile && <Logo />}
+      <h3 className={s.text}>Are you sure you want to log out?</h3>
+      <div className={s.btns}>
+        <FormButton
+          type="button"
+          text={'Logout'}
+          variant={'multiColorButtton'}
+          handlerFunction={() => dispatch(signOutThunk())}
+        />
 
-          <FormButton
-            type="button"
-            text={'Cancel'}
-            variant={'whiteButtton'}
-            handlerFunction={onClose}
-          />
-        </div>
+        <FormButton
+          type="button"
+          text={'Cancel'}
+          variant={'whiteButtton'}
+          handlerFunction={onClose}
+        />
       </div>
-    </Modal>
+    </div>
   );
 };
 
