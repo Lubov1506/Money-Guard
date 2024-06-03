b50226e3c55417c326158886409a7e45fd0d87cd
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2024-06-03 09:18:12 +0300

    fix datePicker

--- src/components/StatisticDatePicker/StatisticDatePicker.jsx
+++ src/components/StatisticDatePicker/StatisticDatePicker.jsx
@@ -5,6 +5,7 @@ import { getYear } from 'date-fns';
 import Select from 'react-select';
 import { useDispatch } from 'react-redux';
 import { fetchPeriodTrnThunk } from '../../redux/transactions/operations';
+import { datePickerStyles } from './datePickerStyles';
 
 const StatisticDatePicker = () => {
   const currentMonth = new Date().getMonth() + 1;
@@ -29,61 +30,21 @@ const StatisticDatePicker = () => {
       isDisabled: i + 1 > currentMonth && selectedYear === currentYear,
     };
   });
-  const customStyles = {
-    control: (baseStyles, state) => ({
-      ...baseStyles,
-      borderColor: state.isFocused ? 'grey' : ' rgba(74, 86, 226, 0.10)',
-      backgroundColor: 'transparent',
-      color: 'white',
-      borderCollapse: 'separate',
-    }),
-    dropdownIndicator: baseStyles => ({
-      ...baseStyles,
-      color: 'white',
-    }),
-    menu: baseStyles => ({
-      ...baseStyles,
-      background:
-        'linear-gradient(180deg, #855daf99 13%, #6A46A5 53%, #50309A 70%, #6A46A5 100%)',
-      borderCollapse: 'separate',
-      borderRadius: '8px',
-      boxShadow: '0px 4px 60px 0px rgba(0, 0, 0, 0.25)',
-      backdropFilter: 'blur(50px)',
-    }),
-    option: (baseStyles, state) => ({
-      ...baseStyles,
-      backgroundColor: state.isFocused ? '#ffffff1a' : 'transparent',
-      color: state.isSelected ? '#FF868D' : 'white',
-    }),
-    singleValue: baseStyles => ({
-      ...baseStyles,
-      color: 'white',
-    }),
-
-    indicatorSeparator: baseStyles => ({
-      ...baseStyles,
-      display: 'none',
-    }),
-    valueContainer: baseStyles => ({
-      ...baseStyles,
-      padding: '12px 0px 12px 20px',
-    }),
-
-    cursor: 'pointer',
-  };
 
   return (
-    <div className={s.monthYearPick}>
+    <div className={s.monthYearPick_wrapper}>
       <Select
-        defaultValue={currentYear}
-        styles={customStyles}
+        defaultValue={{ value: currentYear, label: currentYear }}
+        styles={datePickerStyles}
+        className={s.monthYearPick}
         onChange={selectedOption => setSelectedYear(selectedOption.value)}
         openMenuOnClick={true}
         placeholder="Select year"
         options={yearOptions}
       />
       <Select
-        styles={customStyles}
+        styles={datePickerStyles}
+        className={s.monthYearPick}
         onChange={selectedOption => setSelectedMonth(selectedOption.value)}
         options={monthsOptions}
         placeholder="Select month"
