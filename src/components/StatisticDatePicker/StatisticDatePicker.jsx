5262d0c09fa0dacf30843e72888e93114dc4c5f9
AnnetaDe <anneta.liss@gmail.com>
2024-06-03 11:20:32 +0200

    3

--- src/components/StatisticDatePicker/StatisticDatePicker.jsx
+++ src/components/StatisticDatePicker/StatisticDatePicker.jsx
@@ -30,48 +30,48 @@ const StatisticDatePicker = () => {
       isDisabled: i + 1 > currentMonth && selectedYear === currentYear,
     };
   });
-  const customStyles = {
-    control: (baseStyles, state) => ({
-      ...baseStyles,
-      borderColor: state.isFocused ? 'grey' : ' rgba(74, 86, 226, 0.10)',
-      backgroundColor: 'transparent',
-      color: 'white',
-      borderCollapse: 'separate',
-    }),
-    dropdownIndicator: baseStyles => ({
-      ...baseStyles,
-      color: 'white',
-    }),
-    menu: baseStyles => ({
-      ...baseStyles,
-      background:
-        'linear-gradient(180deg, #855daf99 13%, #6A46A5 53%, #50309A 70%, #6A46A5 100%)',
-      borderCollapse: 'separate',
-      borderRadius: '8px',
-      boxShadow: '0px 4px 60px 0px rgba(0, 0, 0, 0.25)',
-      backdropFilter: 'blur(50px)',
-    }),
-    option: (baseStyles, state) => ({
-      ...baseStyles,
-      backgroundColor: state.isFocused ? '#ffffff1a' : 'transparent',
-      color: state.isSelected ? '#FF868D' : 'white',
-    }),
-    singleValue: baseStyles => ({
-      ...baseStyles,
-      color: 'white',
-    }),
+  // const customStyles = {
+  //   control: (baseStyles, state) => ({
+  //     ...baseStyles,
+  //     borderColor: state.isFocused ? 'grey' : ' rgba(74, 86, 226, 0.10)',
+  //     backgroundColor: 'transparent',
+  //     color: 'white',
+  //     borderCollapse: 'separate',
+  //   }),
+  //   dropdownIndicator: baseStyles => ({
+  //     ...baseStyles,
+  //     color: 'white',
+  //   }),
+  //   menu: baseStyles => ({
+  //     ...baseStyles,
+  //     background:
+  //       'linear-gradient(180deg, #855daf99 13%, #6A46A5 53%, #50309A 70%, #6A46A5 100%)',
+  //     borderCollapse: 'separate',
+  //     borderRadius: '8px',
+  //     boxShadow: '0px 4px 60px 0px rgba(0, 0, 0, 0.25)',
+  //     backdropFilter: 'blur(50px)',
+  //   }),
+  //   option: (baseStyles, state) => ({
+  //     ...baseStyles,
+  //     backgroundColor: state.isFocused ? '#ffffff1a' : 'transparent',
+  //     color: state.isSelected ? '#FF868D' : 'white',
+  //   }),
+  //   singleValue: baseStyles => ({
+  //     ...baseStyles,
+  //     color: 'white',
+  //   }),
 
-    indicatorSeparator: baseStyles => ({
-      ...baseStyles,
-      display: 'none',
-    }),
-    valueContainer: baseStyles => ({
-      ...baseStyles,
-      padding: '16px 0px 16px 20px',
-    }),
+  //   indicatorSeparator: baseStyles => ({
+  //     ...baseStyles,
+  //     display: 'none',
+  //   }),
+  //   valueContainer: baseStyles => ({
+  //     ...baseStyles,
+  //     padding: '16px 0px 16px 20px',
+  //   }),
 
-    cursor: 'pointer',
-  };
+  //   cursor: 'pointer',
+  // };
 
   return (
     <div className={s.monthYearPick_wrapper}>
